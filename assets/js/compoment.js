38a10b98d9df241e4987fa3c14cd3b788170037f
Tuan Khoi <9tuankhoi6@gmail.com>
2025-10-26 22:23:47 +0700

    Thêm chuyển hướng trang cá nhân, vẽ form đăng ký đăng nhập trên index

--- assets/js/compoment.js
+++ assets/js/compoment.js
@@ -49,46 +49,89 @@ sliderContainer.addEventListener('moveleave',()=>{
 
 // Dropdown menu cho tài khoản khi trỏ chuột vào ô Tài khoản
 const account = document.querySelector('.acount-icon');
+const usernameHeader = account.querySelector("span");
 let dropdownMenu = document.createElement("div");
 dropdownMenu.setAttribute("class", "dropdown-menu")
 dropdownMenu.style.visibility="hidden";
-account.appendChild(dropdownMenu);
-dropdownMenu.innerHTML=`
-                        <ul>
-                            <li><button id="sign-in">Đăng nhập</button></li>
-                            <li><button id="sign-up">Đăng ký</button></li>
-                        </ul>
-                        `;
-
+drawDropdownMenu();
 function drawDropdownMenu() 
 {                     
-    if (sessionStorage.getItem("login-status") !== null)
+    if (sessionStorage.getItem("login-status") != null)
     {
         dropdownMenu.innerHTML = `
                     <ul>
-                        <li><button>Thông tin cá nhân</button></li>
-                        <li><button id="sign-out">Đăng xuất</button></li>
+                        <li><button id="profile">Thông tin cá nhân</button></li>
                         <li><button>Đổi mật khẩu</button></li>
+                        <li><button id="sign-out">Đăng xuất</button></li>
                     </ul>`;
         let list = dropdownMenu.querySelector("ul");
         let listItems = dropdownMenu.querySelectorAll("li");
         let authLink = dropdownMenu.querySelector("a");
         listItems.forEach((li) => {
             li.style.width="fit-content";
-    });
-    list.style.flexDirection="column";
-    list.style.alignItems="flex-end";
-    dropdownMenu.style.minWidth="fit-content";
-    dropdownMenu.style.width="150%";
+        });
+        list.style.flexDirection="column";
+        list.style.alignItems="flex-end";
+        dropdownMenu.style.minWidth="fit-content";
+        dropdownMenu.style.width="150%";
+        account.appendChild(dropdownMenu);
+        usernameHeader.textContent = "Nguyễn Văn A";
+
+        //Thông tin cá nhân
+        const profile = document.getElementById("profile");
+        if (profile != null)
+        { 
+            profile.addEventListener("click", (event) => {
+                location.href="auth.html";
+            })
+        }
+        //Đăng xuất
+        const signout = document.getElementById("sign-out");
+        if (signout != null)
+        {
+            signout.addEventListener("click", (event) => {
+            console.log("sign out");
+            sessionStorage.removeItem("login-status");
+            console.log(sessionStorage.getItem("login-status"));
+            drawDropdownMenu();
+            });
+        }
     }
     else
     {
+        console.log("get in");
         dropdownMenu.innerHTML=`
                         <ul>
                             <li><button id="sign-in">Đăng nhập</button></li>
                             <li><button id="sign-up">Đăng ký</button></li>
                         </ul>
                         `;
+        account.appendChild(dropdownMenu);
+        usernameHeader.textContent = "Tài khoản";
+
+        //Đăng nhập
+        const signin = document.getElementById("sign-in");
+        if (signin != null)
+        {
+            signin.addEventListener("click", (event) => {
+                console.log("sign in");
+                sessionStorage.setItem("login-status", "true"); 
+                drawFormDangNhap();
+                drawDropdownMenu();
+            });
+        }
+
+        //Đăng ký
+        const signup = document.getElementById("sign-up");
+        if (signup != null)
+        {
+            signup.addEventListener("click", (event) => {
+                console.log("sign up");
+                // sessionStorage.setItem("login-status", "true"); 
+                drawFormDangKy();
+                drawDropdownMenu();
+            });
+        }
     }
 }
 
@@ -99,24 +142,4 @@ account.addEventListener("mouseover", (event) => {
 account.addEventListener("mouseout", (event) => {
     dropdownMenu.style.visibility="hidden";
 });
-
-
-const signin = document.getElementById("sign-in");
-if (signin !== null)
-{
-    signin.addEventListener("click", (event) => {
-        console.log("sign in");
-        sessionStorage.setItem("login-status", "true"); 
-        drawDropdownMenu();
-    });
-}
-
-const signout = document.getElementById("sign-out");
-if (signout === null)
-{
-    signout.addEventListener("click", (event) => {
-    console.log("sign out");
-    sessionStorage.removeItem("login-status");
-});
-}
 // ================Product==============
