838c9563d049b75cdfa0db2365392fdedbde45c7
Tuan Khoi <9tuankhoi6@gmail.com>
2025-10-26 22:11:51 +0700

    Thêm drop down menu, hiện form đăng nhập đăng ký tùy theo input người dùng
    Form đăng nhập và đăng ký đã có validation đầy đủ

--- assets/js/compoment.js
+++ assets/js/compoment.js
@@ -37,7 +37,7 @@ function resetAutoSilde(){
     autoSlideInterval =setInterval(autoSlide,3600);
 }
 // không cần ALL vì chỉ có 1 
-const sliderContainer = document.querySelector('slider-container');
+const sliderContainer = document.querySelector('.slider-container');
 //addEventListener(kiểu nghe, người nghe , option)
 sliderContainer.addEventListener('mouseenter',() => {
     clearInterval(autoSlideInterval);
@@ -47,4 +47,76 @@ sliderContainer.addEventListener('moveleave',()=>{
     autoSlideInterval = setInterval(autoSlide,3600);
 })
 
+// Dropdown menu cho tài khoản khi trỏ chuột vào ô Tài khoản
+const account = document.querySelector('.acount-icon');
+let dropdownMenu = document.createElement("div");
+dropdownMenu.setAttribute("class", "dropdown-menu")
+dropdownMenu.style.visibility="hidden";
+account.appendChild(dropdownMenu);
+dropdownMenu.innerHTML=`
+                        <ul>
+                            <li><button id="sign-in">Đăng nhập</button></li>
+                            <li><button id="sign-up">Đăng ký</button></li>
+                        </ul>
+                        `;
+
+function drawDropdownMenu() 
+{                     
+    if (sessionStorage.getItem("login-status") !== null)
+    {
+        dropdownMenu.innerHTML = `
+                    <ul>
+                        <li><button>Thông tin cá nhân</button></li>
+                        <li><button id="sign-out">Đăng xuất</button></li>
+                        <li><button>Đổi mật khẩu</button></li>
+                    </ul>`;
+        let list = dropdownMenu.querySelector("ul");
+        let listItems = dropdownMenu.querySelectorAll("li");
+        let authLink = dropdownMenu.querySelector("a");
+        listItems.forEach((li) => {
+            li.style.width="fit-content";
+    });
+    list.style.flexDirection="column";
+    list.style.alignItems="flex-end";
+    dropdownMenu.style.minWidth="fit-content";
+    dropdownMenu.style.width="150%";
+    }
+    else
+    {
+        dropdownMenu.innerHTML=`
+                        <ul>
+                            <li><button id="sign-in">Đăng nhập</button></li>
+                            <li><button id="sign-up">Đăng ký</button></li>
+                        </ul>
+                        `;
+    }
+}
+
+account.addEventListener("mouseover", (event) => {
+    dropdownMenu.style.visibility="visible";
+});
+
+account.addEventListener("mouseout", (event) => {
+    dropdownMenu.style.visibility="hidden";
+});
+
+
+const signin = document.getElementById("sign-in");
+if (signin !== null)
+{
+    signin.addEventListener("click", (event) => {
+        console.log("sign in");
+        sessionStorage.setItem("login-status", "true"); 
+        drawDropdownMenu();
+    });
+}
+
+const signout = document.getElementById("sign-out");
+if (signout === null)
+{
+    signout.addEventListener("click", (event) => {
+    console.log("sign out");
+    sessionStorage.removeItem("login-status");
+});
+}
 // ================Product==============
