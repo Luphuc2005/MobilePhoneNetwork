60cef17238e2fba62fa0eeaec5bda3a2e8defc62
duyddawngnef <duyddang178@gmail.com>
2025-10-28 22:36:19 +0700

    Revert "@NgThanhNhanf done detail orders"
    This reverts commit a1641f6ed7670fe7eb63007025f738daeae9aadb, reversing
    changes made to 0570f8fae8e795745c0cb7b1bfa6b78cc32c6497.

--- assets/js/quanlynguoidung.js
+++ assets/js/quanlynguoidung.js
@@ -14,8 +14,6 @@ function navigateTo(section, event) {
     document.getElementById("dashboard-content").style.display = "block";
   } else if (section === "customers") {
     document.getElementById("customers-content").style.display = "block";
-  } else if (section === "orders") {
-    document.getElementById("orders-id").style.display = "block";
   } else if (section === "pricing") {
     document.getElementById("pricing-content").style.display = "block";
     } else if (section === "products") {
@@ -28,8 +26,6 @@ function navigateTo(section, event) {
         initializePricing();
       }
     }, 100);
-  } else if(section === "inventory") {
-    document.getElementById("inventory-content").style.display = "block";
   } else {
     document.getElementById("other-content").style.display = "block";
   }
