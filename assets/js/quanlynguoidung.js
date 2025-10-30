73e811e0f425234421a882cba3ea52a83f87c887
duyddawngnef <duyddang178@gmail.com>
2025-10-30 19:17:25 +0700

    Revert "Merge branch 'main' into hphucnew"
    This reverts commit a0b613b3f84ba8cf7083983a082ac201011d3baf, reversing
    changes made to 8458ed52600e9a0945e1db643632148bd17aa3e0.

--- assets/js/quanlynguoidung.js
+++ assets/js/quanlynguoidung.js
@@ -26,11 +26,6 @@ function navigateTo(section, event) {
         initializePricing();
       }
     }, 100);
-  } else if (section === "orders") {
-    document.getElementById("orders-content").style.display = "block";
-    console.log(document.getElementById("orders-id"));
-  } else if(section === "inventory") {
-    document.getElementById("inventory-content").style.display = "block";
   } else {
     document.getElementById("other-content").style.display = "block";
   }
