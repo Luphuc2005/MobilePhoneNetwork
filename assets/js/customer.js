4ab858feb8e0f15729b8e9571b23ebc9b7c1a7a1
duyddawngnef <duyddang178@gmail.com>
2025-11-03 12:03:07 +0700

    Revert "@duyddawngnef Revert "Merge branch 'feature/duyddawng-struct"
    This reverts commit 5d9f3417bef92f2db25ead120ba14e38a5acc845, reversing
    changes made to 3f287345b477d84a4de5d28c399a7d79afdac1c7.

--- assets/js/customer.js
+++ assets/js/customer.js
@@ -45,7 +45,7 @@ function storeCustomerInLocalStorage() {
 
 // Lấy customer trong local storage
 function getAllCustomer() {
-    return JSON.parse(localStorage.getItem('allCustomer'));
+    return JSON.parse(localStorage.getItem('phonestore_users'));
 }
 
 //--------------------MAIN--------------------------//
